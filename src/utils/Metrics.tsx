20fde4b8108995302ae0de11a46f269d8b5bb5ae
purevsurena <a.purevsuren0120@gmail.com>
2023-11-14 12:54:07 +0800

    feat: 🎸 Add Scalability Helpers for Responsive Design in RN

---
+++ src/utils/Metrics.tsx
@@ -0,0 +1,14 @@
+import { Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+
+const guidelineBaseWidth = 375;
+
+// width, height, margin, padding, likewise
+const scale = (size: number) => (width / guidelineBaseWidth) * size;
+
+// for font-size, border-radius, likewise
+const mScale = (size: number, factor = 0.5) =>
+  size + (scale(size) - size) * factor;
+
+export { scale, mScale };
