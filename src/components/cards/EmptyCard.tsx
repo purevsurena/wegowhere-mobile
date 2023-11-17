467bed721a95f08c7afa57b6c8eb30154d0bd8b0
purevsurena <a.purevsuren0120@gmail.com>
2023-11-18 02:20:41 +0800

    feat: 🎸 Implemented empty-card component UI with tests

---
+++ src/components/cards/EmptyCard.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { WhiteSpace, Click, Flex } from '@/components/layouts';
+import Icons from '@/components/common/images/Icons';
+import { Heading, Paragraph } from '@/components/common/typography';
+
+const EmptyCard = () => {
+  return (
+    <Flex flex={1} jc="center" ai="center">
+      <Icons name="visa" width={40} height={40} />
+      <WhiteSpace size="17" />
+
+      <Paragraph type="Paragraph1" label="No Cards Found" />
+      <WhiteSpace size="14" />
+
+      <Paragraph
+        type="Paragraph1"
+        label={'We recommend adding a card\n for easy payment'}
+        textAlign="center"
+      />
+      <WhiteSpace size="14" />
+
+      <Click onPress={() => {}}>
+        <Heading type="Heading2" label="Add New Card" color="primary" />
+      </Click>
+    </Flex>
+  );
+};
+
+export default EmptyCard;
---
+++ src/components/cards/__test__/EmptyCard.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import EmptyCard from '@/components/cards/EmptyCard';
+
+describe('<EmptyCard />', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<EmptyCard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
