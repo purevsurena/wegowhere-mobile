a3a172739835df5cff0724806fca14ed5bb8a70f
purevsurena <a.purevsuren0120@gmail.com>
2023-11-18 02:17:06 +0800

    feat: 🎸 added index.tsx file for layout components

---
+++ src/components/layouts/index.tsx
@@ -0,0 +1,7 @@
+import Flex from '@/components/layouts/Flex';
+import WhiteSpace from '@/components/layouts/WhiteSpace';
+import WingBlank from '@/components/layouts/WingBlank';
+import Click from '@/components/layouts/Click';
+import Screen from '@/components/layouts/Screen';
+
+export { Flex, WhiteSpace, WingBlank, Click, Screen };
