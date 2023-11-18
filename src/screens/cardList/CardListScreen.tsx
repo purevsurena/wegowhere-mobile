ab35523232acb559ebde89ee2d8db9ec2d3b667e
purevsurena <a.purevsuren0120@gmail.com>
2023-11-18 22:39:39 +0800

    feat: 🎸 Implemented Dummy Logic for CardListScreen

--- src/screens/cardList/CardListScreen.tsx
+++ src/screens/cardList/CardListScreen.tsx
@@ -1,13 +1,49 @@
 import * as React from 'react';
-import { Screen } from '@/components/layouts';
+import { Screen, WhiteSpace, WingBlank } from '@/components/layouts';
 import EmptyCard from '@/components/cards/EmptyCard';
+import Card from '@/components/cards/Card';
+import { ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+
+const dummyCards = [
+  {
+    id: Math.random(),
+    cardHolderName: 'Ty Lee',
+    last4Digits: '3282',
+    expirationDate: '12/25',
+  },
+];
 
 const CardListScreen = () => {
+  const [cards] = React.useState(dummyCards);
   return (
-    <Screen title="Cards" flex={1} jc="center" ai="center">
-      <EmptyCard />
+    <Screen title="Cards" flex={1} ai="center">
+      {cards.length === 0 && <EmptyCard />}
+      {cards.length > 0 && (
+        <ScrollView style={styles.scrollViewContainer}>
+          <WhiteSpace size="14" />
+          <WingBlank size="22">
+            {cards.map(card => (
+              <Card
+                key={card.id}
+                cardHolderName={card.cardHolderName}
+                last4Digits={card.last4Digits}
+                expirationDate={card.expirationDate}
+                onPress={() => {}}
+              />
+            ))}
+          </WingBlank>
+        </ScrollView>
+      )}
     </Screen>
   );
 };
 
 export default CardListScreen;
+
+const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flex: 1,
+    width: '100%',
+  },
+});
